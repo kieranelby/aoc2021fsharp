d25d8caba16426de08cb376ac36b13eb41516d3e
Kieran Elby <kieran@dunelm.org.uk>
2021-12-15 22:22:12 +0000

    days 14 and 15

---
+++ ConsoleApp14/Program.fs
@@ -0,0 +1,108 @@
+﻿// Advent of Code 2021 - F# noob
+
+//let fileName = "example.txt"
+let fileName = "input.txt"
+let inputLines =
+    System.IO.File.ReadLines(fileName)
+        |> Seq.cast<string>
+        |> Seq.map (fun l -> l.Trim())
+
+let parsePairInsertionRule (line : string) =
+    let parts = line.Split(" -> ")
+    (parts[0], parts[1])
+
+let polymerTemplate = inputLines |> Seq.head
+
+let pairInsertionRules =
+    inputLines
+        |> Seq.skipWhile (fun l -> l.Contains " -> " |> not)
+        |> Seq.map parsePairInsertionRule
+        |> Map.ofSeq
+
+// part one
+
+let applyRulesOnce rules (template : string) =
+    let len = template.Length
+    let firsts = template.Substring(0, len-1).ToCharArray()
+    let seconds = template.Substring(1, len-1).ToCharArray()
+    let folder acc first second =
+        let pair = $"{first}{second}"
+        let insertion =
+            match Map.tryFind pair rules with
+            | Some x -> x
+            | None -> ""
+        $"{acc}{first}{insertion}"
+    let nearlyRight = Seq.fold2 folder "" firsts seconds
+    $"{nearlyRight}{Seq.last seconds}"
+
+let rec applyRulesN n rules template =
+    //printfn $"""n=%A{n} template=%A{template}"""
+    if n = 0 then
+        template
+    else
+        let template' = applyRulesOnce rules template
+        applyRulesN (n-1) rules template'
+
+let leastCommonQuantity polymer =
+    polymer
+        |> Seq.countBy id
+        |> Seq.map snd
+        |> Seq.min
+
+let mostCommonQuantity polymer =
+    polymer
+        |> Seq.countBy id
+        |> Seq.map snd
+        |> Seq.max
+    
+let partOne =
+    let polymer = applyRulesN 10 pairInsertionRules polymerTemplate
+    (mostCommonQuantity polymer) - (leastCommonQuantity polymer)
+
+printfn $"""partOne=%A{partOne}"""
+
+// part two
+
+// 'orrbile hack; add a bogus char at the start so at the end when we map back from
+// counts of pairs to counts of elements we can avoid double-counting overlapping
+// pairs by simply only considering each pair as the second element (if we didn't
+// add the bogus char at the start then we would undercount the first char!)
+let polymerPairCounts =
+    $"*{polymerTemplate}"
+        |> Seq.pairwise
+        |> Seq.map (fun (a,b) -> $"{a}{b}")
+        |> Seq.countBy id
+        |> Seq.map (fun (pair, count) -> (pair, count |> bigint))
+
+let applyCountRulesOnce rules (templatePairCounts : seq<string * bigint>) =
+    templatePairCounts
+        |> Seq.collect (
+            fun (pair, count) ->
+                match Map.tryFind pair rules with
+                | Some x -> seq { ($"{pair.Chars(0)}{x}", count) ; ($"{x}{pair.Chars(1)}", count) }
+                | None -> seq { (pair,count) })
+        |> Seq.groupBy fst
+        |> Seq.map (fun (a, entries) -> (a, (entries |> Seq.map snd |> Seq.sum)) )
+
+let rec applyCountRulesN n rules templatePairCounts =
+    printfn $"""n=%A{n} template=%A{templatePairCounts} sum={templatePairCounts |> Seq.map snd |> Seq.sum}"""
+    if n = 0 then
+        templatePairCounts
+    else
+        let templatePairCounts' = applyCountRulesOnce rules templatePairCounts
+        applyCountRulesN (n-1) rules templatePairCounts'
+
+let pairCountsToElementCounts (templatePairCounts : seq<string * bigint>) =
+    templatePairCounts
+        |> Seq.map (fun (pair, count) -> (pair.Chars(1), count))
+        |> Seq.groupBy fst
+        |> Seq.map (fun (a, entries) -> (a, (entries |> Seq.map snd |> Seq.sum)) )
+
+let partTwo =
+    let pairCounts = applyCountRulesN 40 pairInsertionRules polymerPairCounts
+    printfn $"""%A{pairCounts |> pairCountsToElementCounts}"""
+    let mostCommon = pairCounts |> pairCountsToElementCounts |> Seq.map snd |> Seq.max
+    let leastCommon = pairCounts |> pairCountsToElementCounts |> Seq.map snd |> Seq.min
+    mostCommon - leastCommon
+
+printfn $"""partTwo=%A{partTwo}"""
---
+++ ConsoleApp15/Program.fs
@@ -0,0 +1,98 @@
+﻿// Advent of Code 2021 - F# noob
+
+//let fileName = "example.txt"
+let fileName = "input.txt"
+let inputLines =
+    System.IO.File.ReadLines(fileName)
+        |> Seq.cast<string>
+        |> Seq.map (fun l -> l.Trim())
+
+let riskLevelArray =
+    inputLines |> Seq.map (
+        fun l -> l.ToCharArray() |> Seq.map (fun c -> int c - int '0') )
+    |> array2D
+
+type Path = int * List<int * int>
+
+let endOf (riskLevels : int[,]) = (riskLevels.GetLength(0)-1, riskLevels.GetLength(1)-1)
+
+let isAtEnd (riskLevels : int[,]) (path : Path) =
+    let (_, cs) = path
+    let lc = Seq.head cs
+    lc = endOf riskLevels
+
+let generatePaths (riskLevels : int[,]) (path : Path) =
+    let (s, cs) = path
+    let lc = List.head cs
+    let (lc0, lc1) = lc
+    let ncs =
+        seq {
+           (lc0-1,lc1)
+           (lc0+1,lc1)
+           (lc0,lc1-1)
+           (lc0,lc1+1) }
+        |> Seq.filter (
+            fun (c0, c1) ->
+                c0 >= 0 && c0 < riskLevels.GetLength(0) &&
+                c1 >= 0 && c1 < riskLevels.GetLength(1) )
+    ncs
+        |> Seq.map (fun nc ->
+            Path(s + riskLevels[fst nc, snd nc], nc :: cs) )
+        |> List.ofSeq
+
+let keepBestPaths (riskLevels : int[,]) (bestScores : Map<(int * int),int>) (paths : List<Path>) =
+    paths
+        |> List.groupBy (fun (_, cs) -> cs |> List.head)
+        |> List.map (fun (_, ps) -> ps |> List.sortBy fst |> List.head)
+        |> List.filter (fun (s, cs) ->
+            let last = cs |> List.head
+            let maybeBest = Map.tryFind last bestScores
+            match maybeBest with
+            | Some best -> s < best
+            | None -> true )
+
+let updateBestScores (bestScores : Map<(int * int),int>) (keptPaths : List<Path>) =
+    let betterScores =
+        keptPaths
+            |> Seq.map (fun (s, cs) -> (List.head cs, s))
+            |> Map.ofSeq
+    Map.fold (fun acc key value -> Map.add key value acc) bestScores betterScores
+
+let rec search (riskLevels : int[,]) (currentPaths : List<Path>) (bestScores : Map<(int * int),int>) =
+    if List.isEmpty currentPaths then
+        match Map.tryFind (endOf riskLevels) bestScores with
+        | Some score -> score
+        | None -> failwith "ran out of paths before finding end"
+    else
+        let currentPaths' =
+            currentPaths
+                |> List.collect (generatePaths riskLevels)
+                |> keepBestPaths riskLevels bestScores
+        let bestScores' = updateBestScores bestScores currentPaths'
+        search riskLevels currentPaths' bestScores'
+
+let partOne =
+    let currentPaths = [ Path(0, [(0,0)]) ]
+    let bestScores = seq { ((0,0), 0) } |> Map.ofSeq
+    search riskLevelArray currentPaths bestScores
+
+printfn $"""partOne=%A{partOne}"""
+
+let giantRiskLevelArray =
+    let l0 = riskLevelArray.GetLength(0)
+    let l1 = riskLevelArray.GetLength(1)
+    Array2D.init (l0 * 5) (l1 * 5) (
+        fun i j ->
+            let ri = i % l0
+            let rj = j % l1
+            let di = i / l0
+            let dj = j / l1
+            let original = riskLevelArray[ri, rj]
+            1 + (original + di + dj - 1) % 9 )
+
+let partTwo =
+    let currentPaths = [ Path(0, [(0,0)]) ]
+    let bestScores = seq { ((0,0), 0) } |> Map.ofSeq
+    search giantRiskLevelArray currentPaths bestScores
+
+printfn $"""partTwo=%A{partTwo}"""
